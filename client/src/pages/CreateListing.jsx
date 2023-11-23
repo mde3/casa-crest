377b807d3a69ebd3e0f0039032d640e7b8be087a
Mabior Duom <mabiorduom012@gmail.com>
2023-11-24 01:53:37 +0300

    feat: complete listing functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -7,8 +7,12 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../firebase/firebase';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateListing() {
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
   const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState({
     imageUrls: [],
@@ -26,8 +30,8 @@ export default function CreateListing() {
   });
   const [imageUploadError, setImageUploadError] = useState(false);
   const [uploading, setUploading] = useState(false);
-  // const [error, setError] = useState(false);
-  // const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleImageSubmit = (e) => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
@@ -89,16 +93,76 @@ export default function CreateListing() {
     });
   };
 
+  const handleChange = (e) => {
+    if (e.target.id === 'sale' || e.target.id === 'rent') {
+      setFormData({
+        ...formData,
+        type: e.target.id,
+      });
+    }
+
+    if (
+      e.target.id === 'parking' ||
+      e.target.id === 'furnished' ||
+      e.target.id === 'offer'
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.checked,
+      });
+    }
+
+    if (
+      e.target.type === 'number' ||
+      e.target.type === 'text' ||
+      e.target.type === 'textarea'
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError('You must upload at least one image');
+      if (+formData.regularPrice < +formData.discountPrice)
+        return setError('Discount price must be lower than regular price');
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/listing/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+        setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+      navigate(`/listing/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
   return <Helmet title={"Create Listing"}>
     <section className="flex min-h-full flex-1 flex-col justify-center px-6 py-6 lg:px-8">
-    {/* <section className="max-w-7xl mx-auto p-6 px-4 lg:px-8 xl:max-w-full"> */}
-      
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Create Listing
         </h2>
       </div>
-      <form className="space-y-6">
+      <form onSubmit={handleSubmit} className="space-y-6">
         <div className="flex flex-col lg:flex-row gap-4">
           <div className='flex flex-col lg:w-6/12'>
             <div className="mt-4">
@@ -112,6 +176,8 @@ export default function CreateListing() {
                   maxLength='62'
                   minLength='10'
                   required
+                  onChange={handleChange}
+                  value={formData.name}
                   className="block w-full rounded-md border-0 outline-0 py-2.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -125,6 +191,8 @@ export default function CreateListing() {
                   type="text"
                   id="description"
                   required
+                  onChange={handleChange}
+                  value={formData.description}
                   className="block w-full rounded-md border-0 outline-0 py-2.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -138,6 +206,8 @@ export default function CreateListing() {
                   type="text"
                   id="address"
                   required
+                  onChange={handleChange}
+                  value={formData.address}
                   className="block w-full rounded-md border-0 outline-0 py-2.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -147,7 +217,8 @@ export default function CreateListing() {
                 <input
                   type='checkbox'
                   id="sale"
-                  required
+                  onChange={handleChange}
+                  checked={formData.type === 'sale'}
                   className="block rounded-md border-0 outline-0 py-2.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 <span>Sale</span>
@@ -156,7 +227,8 @@ export default function CreateListing() {
                 <input
                   type='checkbox'
                   id="rent"
-                  required
+                  onChange={handleChange}
+                  checked={formData.type === 'rent'}
                   className="block rounded-md border-0 outline-0 py-2.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 <span>Rental</span>
@@ -165,7 +237,8 @@ export default function CreateListing() {
                 <input
                   type='checkbox'
                   id="furnished"
-                  required
+                  onChange={handleChange}
+                  checked={formData.furnished}
                   className="block rounded-md border-0 outline-0 py-2.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 <span>Furnished</span>
@@ -174,7 +247,8 @@ export default function CreateListing() {
                 <input
                   type='checkbox'
                   id="offer"
-                  required
+                  onChange={handleChange}
+                  checked={formData.offer}
                   className="block rounded-md border-0 outline-0 py-2.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 <span>Offer</span>
@@ -183,7 +257,8 @@ export default function CreateListing() {
                 <input
                   type='checkbox'
                   id="parking"
-                  required
+                  onChange={handleChange}
+                  checked={formData.parking}
                   className="block rounded-md border-0 outline-0 py-2.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 <span>Parking spot</span>
@@ -197,6 +272,8 @@ export default function CreateListing() {
                   min='1'
                   max='10'
                   required
+                  onChange={handleChange}
+                  value={formData.bedrooms}
                   className="block rounded-md border-0 outline-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 <span>Bedrooms</span>
@@ -208,6 +285,8 @@ export default function CreateListing() {
                   min='1'
                   max='10'
                   required
+                  onChange={handleChange}
+                  value={formData.bathrooms}
                   className="block rounded-md border-0 outline-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 <span>Bathrooms</span>
@@ -222,30 +301,40 @@ export default function CreateListing() {
                     min='50'
                     max='10000000'
                     required
+                    onChange={handleChange}
+                    value={formData.regularPrice}
                     className="block rounded-md border-0 outline-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                   <div className='flex flex-col items-center'>
                     <p>Regular price</p>
-                    <span className='text-xs'>($ / month)</span>
+                    {formData.type === 'rent' && (
+                      <span className='text-xs'>($ / month)</span>
+                    )}
                   </div>
                 </div>
               </div>
-              <div className="mt-4 flex flex-wrap gap-4">
-                <div className='flex items-center gap-2'>
-                  <input
-                    type='number'
-                    id="discountPrice"
-                    min='0'
-                    max='10000000'
-                    required
-                    className="block rounded-md border-0 outline-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                  <div className='flex flex-col items-center'>
-                    <p>Discount price</p>
-                    <span className='text-xs'>($ / month)</span>
+              {formData.offer && (
+                <div className="mt-4 flex flex-wrap gap-4">
+                  <div className='flex items-center gap-2'>
+                    <input
+                      type='number'
+                      id="discountPrice"
+                      min='0'
+                      max='10000000'
+                      required
+                      onChange={handleChange}
+                      value={formData.discountPrice}
+                      className="block rounded-md border-0 outline-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                    <div className='flex flex-col items-center'>
+                      <p>Discount price</p>
+                      {formData.type === 'rent' && (
+                        <span className='text-xs'>($ / month)</span>
+                      )}
+                    </div>
                   </div>
                 </div>
-              </div>
+              )}
             </div>
           </div>
           <div className='flex flex-col p-2 items-center justify-center bg-gray-50 rounded-md lg:w-6/12'>
@@ -298,11 +387,13 @@ export default function CreateListing() {
           </div>
         </div>
         <button
+          disabled={loading || uploading}
           type="submit"
           className="flex w-6/12 m-auto justify-center rounded-md bg-myblue px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Create Listing
+          {loading ? 'Creating...' : 'Create listing'}
         </button>
+        {error && <p className='text-red-700 text-center text-sm'>{error}</p>}
       </form>
     </section>
   </Helmet>
