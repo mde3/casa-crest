03d762363697eb5a5502a563cc2e17533adb96ae
Mabior Duom <mabiorduom012@gmail.com>
2023-11-27 14:53:56 +0300

    feat: add get listing route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListing, deleteListing, updateListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
+router.get('/get/:id', getListing);
 
 export default router;
