cfb6f99ff4673d80647bb55d1483ad0537e7cb53
Mabior Duom <mabiorduom012@gmail.com>
2023-12-05 15:02:53 +0300

    feat: complete benefits page

--- client/src/pages/Benefits.jsx
+++ client/src/pages/Benefits.jsx
@@ -1,11 +1,79 @@
 import Helmet from '../components/Helmet'
 
-const Benefits = () => {
-    return <Helmet title={"Benefits"}>
-        <section className="Benefits">
-            Benefits
-        </section>
-    </Helmet>
-}
+const LockClosedIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <path fill="none" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"/>
+  </svg>
+);
+const HouseIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <path fill="none" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m2.25 12l8.955-8.955a1.124 1.124 0 0 1 1.59 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
+  </svg>
+);
+const FingerPrintIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <path fill="none" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M15 19.128a9.38 9.38 0 0 0 2.625.372a9.337 9.337 0 0 0 4.121-.952a4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0a3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0a2.625 2.625 0 0 1 5.25 0Z"/>
+  </svg>
+);
+const KeyIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <path fill="none" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"/>
+  </svg>
+);
 
-export default Benefits
+const features = [
+  {
+    name: 'Secure Transactions',
+    description: 'Our platform ensures your transactions are secure, providing peace of mind with every property you buy or sell.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Verified Listings',
+    description: 'All our listings are verified for authenticity to prevent fraudulent activities and ensure a smooth experience.',
+    icon: HouseIcon,
+  },
+  {
+    name: 'Personalized Support',
+    description: 'Our dedicated team of real estate professionals is always ready to assist you whenever you need.',
+    icon: FingerPrintIcon,
+  },
+  {
+    name: 'Easy Access',
+    description: 'Get access to properties from the comfort of your home. Our platform is easy to navigate and user-friendly.',
+    icon: KeyIcon,
+  },
+]
+
+export default function Benefits() {
+  return <Helmet title={"Benefits"}>
+    <section className="py-24">
+      <div className="max-w-7xl mx-auto px-4 lg:px-8 xl:max-w-full">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-base font-semibold leading-7 text-myblue">Benefits</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Here are some of the benefits
+          </p>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Discover the advantages of using our platform for all your real estate needs. From secure 
+            transactions to verified listings, we provide a seamless and efficient experience.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative pl-16 p-4 border border-solid border-gray-300 rounded-lg">
+                <dt className="text-base font-semibold leading-7 text-gray-900">
+                  <div className="absolute left-[8px] top-[8px] flex h-10 w-10 items-center justify-center rounded-lg bg-myblue">
+                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </section>
+  </Helmet>
+}
